6ee00dee9cb38b433bb8028ae019eeda89edea9e
Mohanad Elhag <moh.7afiz@gmail.com>
2019-03-23 16:48:15 +0300

    first commit

---
+++ dist/js/app.js
@@ -0,0 +1,15 @@
+// Scroll to the top function
+
+// $(document).ready(function() {
+//   $(window).scroll(function() {
+//     if ($(this).scrollTop() > 10) {
+//       $("#scroll").fadeIn();
+//     } else {
+//       $("#scroll").fadeOut();
+//     }
+//   });
+//   $("#scroll").click(function() {
+//     $("html, body").animate({ scrollTop: 0 }, 600);
+//     return false;
+//   });
+// });
